69696cb83568185d61fb047f2d67fa9e5f73eaa4
ChaeYoungDo <dudeh534@naver.com>
2016-02-13 12:25:18 +0900

    videoViewExample

---
+++ app/src/main/java/com/youngdo_company/videoexample/MainActivity.java
@@ -0,0 +1,45 @@
+package com.youngdo_company.videoexample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        videoView.setVideoPath("/storage/emulated/0/video.mp4");
+        final MediaController mediaController =
+                new MediaController(this);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
